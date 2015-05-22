28d89ae0279dba75392a02bcc58a777c50e6a002
Skylar Kelty <s.kelty@kent.ac.uk>
2015-05-22 14:45:08 +0100

    Force bootstrap styles

--- renderer.php
+++ renderer.php
@@ -113,7 +113,7 @@ class format_standardweeks_renderer extends format_weeks_renderer
             'class' => 'nopad'
         ));
 
-        echo \html_writer::start_tag('div', array('id' => 'formatbuttons'));
+        echo \html_writer::start_tag('div', array('id' => 'formatbuttons', 'class' => 'bootstrap'));
         echo \html_writer::tag('p',  get_string('emptydesc', 'format_standardweeks'));
 
         echo \html_writer::start_tag('div', array('class' => 'row'));
