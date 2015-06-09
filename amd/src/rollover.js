3392b6eb9588148cd5455de8ccccdfd6ee5fca44
Skylar Kelty <s.kelty@kent.ac.uk>
2015-06-09 15:28:29 +0100

    Update scheduled page

--- amd/src/rollover.js
+++ amd/src/rollover.js
@@ -79,12 +79,17 @@ define([], function() {
     // Begin checking the status every 5 seconds.
     var beginStatusLoop = function() {
         $("#rollovercontainer").html('\
-            <p>Please wait... your course is being rolled over.<br />Note: you can safely close this page and check back later.</p>\
-            <div class="progress">\
-                <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">\
-                    <i class="fa fa-spin fa-spinner"></i>\
+            <p class="lead">Please wait... your course is being rolled over.</p>\
+            <div class="row">\
+                <div class="col-xs-8 col-xs-offset-2">\
+                    <div class="progress">\
+                        <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 15%">\
+                            <i class="fa fa-spin fa-spinner"></i>\
+                        </div>\
+                    </div>\
                 </div>\
             </div>\
+            <p>Note: you can safely close this page and check back later.</p>\
         ');
 
         statusLoop();
